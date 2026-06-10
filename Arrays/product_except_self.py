0bef4c21ad5f7e10f27302f08054e27116d380fd
romit-gitcode <romitp.1401@gmail.com>
2026-06-10 17:38:23 -0400

    contains duplicate

--- Arrays/product_except_self.py
+++ Arrays/product_except_self.py
@@ -14,6 +14,7 @@ def productExceptSelf(nums):
     for i in range(n):
         answer[i] = prefix
         prefix *= nums[i]
+    print("answer1: ", answer)
 
     postfix = 1
     for i in range(n - 1, -1, -1):
@@ -22,5 +23,24 @@ def productExceptSelf(nums):
 
     return answer
 
+# Brute force
+def productExceptSelf2(nums):
+    n=len(nums)
+    products = [1]*n
+    # print(products)
+    for i in range(n):
+        for j in range(n):
+            if( i != j):
+                products[i] *= nums[j]
+            continue
+    return products
+
+def productExceptSelf3(nums):
+    n=len(nums)
+    products = [1]*n
+    
+    
+    
 nums = [1,2,3,4]
+# nums = [2,2,4,5]
 print(productExceptSelf(nums))       
