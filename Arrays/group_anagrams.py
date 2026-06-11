d264a18d333e3ca4f26853037f0c1845ea5051a9
romit-gitcode <romitp.1401@gmail.com>
2026-06-11 12:18:30 -0400

    group anagram without default dict

--- Arrays/group_anagrams.py
+++ Arrays/group_anagrams.py
@@ -6,13 +6,22 @@ strs = ["act","pots","tops","cat","stop","hat"]
 def groupAnagrams(strs):
     res = defaultdict(list)
     for s in strs:
-        str_sort = "".join(sorted(s))
-        res[str_sort].append(s)
+        str_key = "".join(sorted(s))
+        res[str_key].append(s)
     return list(res.values())
 
 # why I had to use default dict here why cant i just use {} directly
 #Ans: You can absolutely use a standard dictionary {} directly! However, if you do, you have to write extra code to manually handle missing keys.If you try to append to a key that doesn't exist yet in a standard dictionary, Python throws a KeyError.
 # See the code below:
- 
+# Solution 2: Brute FOrce without default dict
+def groupAnagrams2(strs):
+    res = {}
+    for s in strs:
+        str_key = "".join(sorted(s))
+        if(str_key not in res):
+            res[str_key] = []
+        res[str_key].append(s)
+    return list(res.values())
 
-print(groupAnagrams(strs))
+
+print(groupAnagrams2(strs))
