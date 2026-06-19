a8468324292f17b7d9ec45d43d5ba611b51dbc58
romit-gitcode <romitp.1401@gmail.com>
2026-06-19 14:30:01 -0400

    encode decode

--- Arrays/encode_decode.py
+++ Arrays/encode_decode.py
@@ -18,8 +18,45 @@ def encode(strs):
         A.append(str(len(word)))
         A.append('#')
         A.append(word)
-    print(A)
     return "".join(A) 
 
-print(encode(strs))
-# print(decode(encode(strs)))
+encoded_string = encode(strs)
+def decode(encoded_string):
+    A = []
+    i = 0
+    while i<len(encoded_string):
+        j=i
+        while encoded_string[j] != '#':
+            j+=1
+        length = int(encoded_string[i:j])
+        i=j+1
+        j = i+length
+        A.append(encoded_string[i:j])
+        i = j
+    return A
+
+def encode1(strs):
+    A=[]
+    for word in strs:
+        A.append(str(len(word)))
+        A.append('#')
+        A.append(word)
+    return "".join(A)
+
+encoded_string2 = encode1(strs)
+print("encoded_string2: ", encoded_string2)
+def decode1(string):
+    A=[]
+    i=0
+    while i<len(string):
+        j = i
+        while string[j] != "#":
+            j+=1
+        num = int(string[i:j])
+        i = j+1
+        j = i + num
+        A.append(string[i:j])
+        i = j
+    return A
+
+print(decode1(encoded_string2))
